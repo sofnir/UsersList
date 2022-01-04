b8fe08ee5fc3b63d9d6c9a4461e3120679563657
Maciej Gwiżdż <mgwizdz@future-processing.com>
2022-01-04 10:39:17 +0100

    create validation dialog

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -3,45 +3,67 @@ import React, { useState } from "react";
 const UserForm = (props) => {
   const [username, setUsername] = useState('');
   const [age, setAge] = useState('');
+  const [showValidationDialog, setShowValidationDialog] = useState(false);
+  const [validationMessage, setValidationMessage] = useState();
 
   const formSubmitHandler = event => {
     event.preventDefault();
 
-    const user = {
-      id: Math.random().toString(),
-      username: username,
-      age: age
-    };
+    if (username === '' || age === '') {
+      setValidationMessage("Please enter a valid name and age (non-empty values).");
+      setShowValidationDialog(true);
+    } else if (age <= 0) {
+      setValidationMessage("Please enter a valid age (> 0).");
+      setShowValidationDialog(true);
+    } else {
+      const user = {
+        id: Math.random().toString(),
+        username: username,
+        age: age
+      };
 
-    props.onAddUser(user);
+      props.onAddUser(user);
 
-    setUsername('');
-    setAge('');
+      setUsername('');
+      setAge('');
+    }
   };
+
+  const closeDialog = () => setShowValidationDialog(false);
 
   const usernameChangeHandler = event => setUsername(event.target.value);
   const ageChangeHandler = event => setAge(event.target.value);
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div>
-        <label>Username</label>
-        <input
-          type="text"
-          value={username}
-          onChange={usernameChangeHandler}
-        />
-      </div>
-      <div>
-        <label>Age (Years)</label>
-        <input
-          type="number"
-          value={age}
-          onChange={ageChangeHandler}
-        />
-      </div>
-      <button type="submit">Add User</button>
-    </form>
+    <div>
+      <form onSubmit={formSubmitHandler}>
+        <div>
+          <label>Username</label>
+          <input
+            type="text"
+            value={username}
+            onChange={usernameChangeHandler}
+          />
+        </div>
+        <div>
+          <label>Age (Years)</label>
+          <input
+            type="number"
+            value={age}
+            onChange={ageChangeHandler}
+          />
+        </div>
+        <button type="submit">Add User</button>
+      </form>
+      <dialog open={showValidationDialog}>
+        <p>
+          {validationMessage}
+          <menu>
+            <button onClick={closeDialog}>Okay</button>
+          </menu>
+        </p>
+      </dialog>
+    </div>
   );
 };
 
